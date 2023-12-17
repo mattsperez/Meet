8c95da3f4a77839da6224110f3eba757e3c877bc
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-12-17 16:08:55 -0600

    atatus-spa added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+// atatus-spa
+import * as atatus from 'atatus-spa';
+atatus.config('49ee928be95044e6b1d4dec3f353a453').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,3 +24,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// atatus-spa test integration
+// atatus.notify(new Error('Test Atatus Setup'));
